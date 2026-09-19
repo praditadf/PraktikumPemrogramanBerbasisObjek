8d531a7edf24ab43fffa573ea58babaf14ae2baf
praditadf <achmadpradita17@gmail.com>
2026-09-19 20:41:36 +0700

    Percobaan 5

---
+++ jobsheet04/percobaan5/Mesin.java
@@ -0,0 +1,13 @@
+package jobsheet04.percobaan5;
+
+public class Mesin {
+    private String tipe;
+
+    public Mesin() {
+        this.tipe = "4-silinder";
+    }
+
+    public String getTipe() {
+        return tipe;
+    }
+}
---
+++ jobsheet04/percobaan5/Mobil.java
@@ -0,0 +1,16 @@
+package jobsheet04.percobaan5;
+
+public class Mobil {
+    private String merek;
+    private Mesin mesin;
+
+    public Mobil(String merek) {
+        this.merek = merek;
+        this.mesin = new Mesin();
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("Mobil: " + merek);
+        System.out.println("Mesin: " + mesin.getTipe());
+    }
+}
