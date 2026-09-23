f78f033494c512f3b67c243dc567cb2a6f502bd2
praditadf <achmadpradita17@gmail.com>
2026-09-23 19:09:57 +0700

    Add Printer atribut and parameter contructor and update MainPercobaan6

--- jobsheet04/percobaan6/Laptop.java
+++ jobsheet04/percobaan6/Laptop.java
@@ -2,13 +2,15 @@ package jobsheet04.percobaan6;
 
 public class Laptop {
     private String merk;
+    private Printer printerDefault;
 
-    public Laptop(String merk) {
+    public Laptop(String merk, Printer printerDefault) {
         this.merk = merk;
+        this.printerDefault = printerDefault;
     }
 
-    public void cetakDokumen(Printer printer, String namaFile) {
+    public void cetakDokumen(String namaFile) {
         System.out.println(merk + " mengirim dokumen ke printer...");
-        printer.cetak(namaFile);
+        printerDefault.cetak(namaFile);
     }
 }
